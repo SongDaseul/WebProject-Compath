271c61bc0869a52f071939628b3cda7d0f96ab58
daseulsong <ektmf836@gmail.com>
2019-05-17 16:10:29 +0900

    test로 올려봅니다.

---
+++ num08.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+#np.polynomial.Polynomial
+p = np.polynomial.Polynomial([-1,2,3])
+print(p)
+plt.show()
+
+
+
+
+
+
+
+
+
